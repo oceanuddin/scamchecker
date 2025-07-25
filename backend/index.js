a474a114837e8eedee1801df89e108d7017fe617
Ocean Uddin <oceanuddin@Oceans-MacBook-Pro.local>
2025-07-25 18:41:35 -0400

    Allow all origins for CORS debugging

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // --- ABSOLUTE FIRST: CORS HANDLER ---
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://scamchecker-beta.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
